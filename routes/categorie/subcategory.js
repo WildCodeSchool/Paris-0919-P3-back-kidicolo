eb8f4f979ed79f119fa057c6c5f88a48d4f694b5
Morgan-Leblanc <morgan.leblanc91@gmail.com>
2019-12-12 18:27:08 +0100

    first commit

---
+++ routes/categorie/subcategory.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const connection = require("../../config/config")
+const router = express.Router()
+
+router.get("/", (req, res) => {
+    connection.query("SELECT * FROM Sub_categorie", (err, results) => {
+      if (err) {
+        res
+          .status(500)
+          .send(`Erreur lors de la récupération de la liste des users !!`)
+      } else {
+        res.status(200).json(results)
+      }
+    })
+  })
+
+router.get("/:id", (req, res) => {
+    const id = req.params.id
+    connection.query("SELECT * FROM Sub_categorie WHERE id_categorie = ?", id, (err, results) => {
+      if (err) {
+        res
+          .status(500)
+          .send(`Erreur lors de la récupération de la liste des users !!`)
+      } else {
+        res.status(200).json(results)
+      }
+    })
+  })
+
+module.exports = router
---
+++ routes/index.js
@@ -0,0 +1,5 @@
+const article = require("./article/article")
+const category = require("./categorie/category")
+const subcategory = require("./categorie/subcategory")
+
+module.exports = { article, category, subcategory }
