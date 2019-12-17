6837be6189ac0c0dcdcbb99ac8e4380677f086d8
Tim <Tim@MacBook-Pro-de-Tim.local>
2019-12-17 14:22:11 +0100

    congiuration ok  back

--- config/config.js
+++ config/config.js
@@ -4,9 +4,10 @@ require('dotenv').config(process.cwd(), '.env')
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PWD,
-    database: process.env.DB_DB,
+    database: process.env.DB_DB
 });
 
 connection.connect(err => {
