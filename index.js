b2195bcb7d1467c91ab4b3ba7317fc1d3df44a5e
Morgan-Leblanc <morgan.leblanc91@gmail.com>
2020-01-08 15:49:44 +0100

    check back for auth

--- index.js
+++ index.js
@@ -2,21 +2,16 @@ const express = require("express")
 const cors = require("cors")
 const morgan = require("morgan")
 const bodyParser = require("body-parser")
-
-const connection = require("./config/config")
 const routes = require("./routes/index")
 
 const PORT = process.env.PORT || "5000"
-
 const app = express()
 
-app.use(cors("* "))
+app.use(cors("*"))
 app.use(morgan("dev"))
-
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-// app.use(express.static(__dirname + "/public"))
 
 app.use("/article", routes.article)
 app.use("/category", routes.category)
--- routes/users/users.js
+++ routes/users/users.js
@@ -1,34 +1,34 @@
 const express = require("express");
 const connection = require("../../config/config");
 const router = express.Router();
-var bcrypt = require('bcryptjs');
-
-
+var bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const jwtSecret = process.env.jwtSecret
 
 router.route("/getinfo/:id").get((req, res) => {
-    const id = req.params.id;
-    connection.query("SELECT * FROM User where id = ?",id, (err, results) => {
-      console.log(err);
-      if (err) {
-        res
-          .status(500)
-          .send(`Erreur lors de la récupération de la liste des users !!`);
-      } else {
-        res.status(200).json(results);
-      }
-    });
+  const id = req.params.id;
+  connection.query("SELECT * FROM User where id = ?", id, (err, results) => {
+    console.log(err);
+    if (err) {
+      res
+        .status(500)
+        .send(`Erreur lors de la récupération de la liste des users !!`);
+    } else {
+      res.status(200).json(results);
+    }
   });
+});
 
 router.route("/signup").post((req, res) => {
   const user = {
     firstname: req.body.firstname,
     mail: req.body.mail,
     lastname: req.body.lastname,
-    password:  bcrypt.hashSync(req.body.password, 10),
+    password: bcrypt.hashSync(req.body.password, 10),
     mobile: req.body.mobile,
     address: req.body.address,
-    photo_user : req.body.photo_user,
-  }
+    photo_user: req.body.photo_user
+  };
   connection.query("INSERT INTO User SET ?", user, (err, results) => {
     console.log(err);
     if (err) {
@@ -41,16 +41,51 @@ router.route("/signup").post((req, res) => {
   });
 });
 
+
+
+
+router.route("/login").post((req, res) => {
+  const mail = req.body.mail
+  const password = req.body.password 
+  connection.query(`SELECT * FROM  User WHERE mail = ?`, mail, (err, result)=>{
+    if (err) {
+      return res.status(500).send(err)
+    } else if (!result[0]){
+      return res.status(401).send('Unauthorized user')
+    }
+    const passwordIsValid = bcrypt.compareSync(password, result[0].password);
+    if (!passwordIsValid){
+      return res.status(401).send({ auth: false, token: null });
+    }else{
+    const token = jwt.sign(
+      {id : result[0].id, firstname: result[0].firstname, mail: result[0].mail},
+      jwtSecret,
+      {
+        expiresIn: '1h'// expires in 1 hours
+      },
+      { algorithm: 'RS256' }
+    );
+    res.header("Access-Control-Expose-Headers", "x-access-token")
+    res.set("x-access-token", token)
+    res.status(200).send({ auth: true })
+    console.log(token)
+  }});
+})
+
+
+  
+  
+
 router.route("/switch/:id").put((req, res) => {
   const user = {
     firstname: req.body.firstname,
     mail: req.body.mail,
     lastname: req.body.lastname,
-    password:  bcrypt.hashSync(req.body.password, 10),
+    password: bcrypt.hashSync(req.body.password, 10),
     mobile: req.body.mobile,
     address: req.body.address,
-    photo_user : req.body.photo_user,
-  }
+    photo_user: req.body.photo_user
+  };
   const id = req.params.id;
   connection.query(
     "UPDATE  User  SET ? WHERE id = ? ",
@@ -69,22 +104,17 @@ router.route("/switch/:id").put((req, res) => {
 });
 
 router.route("/delete/:id").delete((req, res) => {
-
   const id = req.params.id;
-  connection.query(
-    "DELETE FROM User WHERE id = ? ",
-     id,
-    (err, results) => {
-      console.log(err);
-      if (err) {
-        res
-          .status(500)
-          .send(`Erreur lors de la récupération de la liste des users !!`);
-      } else {
-        res.status(200).json(results);
-      }
+  connection.query("DELETE FROM User WHERE id = ? ", id, (err, results) => {
+    console.log(err);
+    if (err) {
+      res
+        .status(500)
+        .send(`Erreur lors de la récupération de la liste des users !!`);
+    } else {
+      res.status(200).json(results);
     }
-  );
+  });
 });
 
 module.exports = router;
