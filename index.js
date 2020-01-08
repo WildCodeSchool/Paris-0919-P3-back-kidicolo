1937b9f7e9b9b7120eabc41eaa4db570a8a908ee
Morgan-Leblanc <morgan.leblanc91@gmail.com>
2020-01-08 10:35:03 +0100

    first modif before new branch for auth

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || "5000"
 
 const app = express()
 
-app.use(cors("*"))
+app.use(cors("* "))
 app.use(morgan("dev"))
 
 app.use(bodyParser.urlencoded({ extended: false }))
