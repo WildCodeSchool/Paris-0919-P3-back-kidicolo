77d5d03e0a0c000804431f33c84a572272203af8
Morgan-Leblanc <morgan.leblanc91@gmail.com>
2020-01-16 10:02:06 +0100

    modif

--- routes/article/article.js
+++ routes/article/article.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 //select all article
 router.get("/", (req, res) => {
-  connection.query("SELECT * FROM Article", (err, results) => {
+  connection.query("SELECT * FROM Article left join Photos on Article.id = Photos.id  ", (err, results) => {
     if (err) {
       res.status(500).send(`Erreur lors de la récupération des articles!`);
     } else {
@@ -14,7 +14,7 @@ router.get("/", (req, res) => {
   });
 });
 
-// get all article form subCat
+// get all article form subCat a refaire!!!
 router.get("/subcat/:id", (req, res) => {
   const idSubCat = req.params.id;
   connection.query(
@@ -48,16 +48,31 @@ router.get("/:id", (req, res) => {
 
 //////////////////////Gérer un article/////////////////////
 router.route("/addarticle").post((req, res) => {
-  const newData = req.body;
-  connection.query("INSERT INTO Article SET ?", newData, (err, results) => {
-    console.log(err);
-    if (err) {
-      res.status(500).send(`Erreur lors de l'ajout de l'article!`);
-    } else {
-      res.status(200).json(results);
+  const body = req.body;
+  connection.query(`INSERT INTO Article (brand , description, price, sold, name, id_state,id_user_acheteur,id_user_vendeur,id_photoart) VALUES (?, ?, ?, ?, ?, ?, ?, ?,?)`, [body.brand, body.description, body.price, body.sold, body.name, body.id_state, body.id_user_acheteur, body.id_user_vendeur, body.id_photoart],
+  err => {
+    if(err){
+      console.log(err)
+      res.send("Error creating article")
     }
-  });
-});
+    else{
+      connection.query(`INSERT INTO Article_categorie VALUES ( (SELECT id from Article where name = ? and brand = ? and description = ?), (SELECT id FROM Categorie WHERE name = ?))`, [body.brand, body.name, body.description],
+      err => {
+        if(err) {
+          res.send ("Error affiliating article to categorie");
+        }else{
+        connection.query(`INSERT INTO Article_subcategorie VALUES ( (SELECT id from Article where name = ? and brand = ? and description = ?), (SELECT id FROM Sub_Categorie WHERE name = ?))`, [body.brand, body.name, body.description],
+        err => {
+          if(err){
+            res.send("Error afiliating article to sub");
+          } else
+          res.send(`Succesfully created article`).status(200)
+        }
+        );
+      }
+    }
+  )
+  }})});
 
 router.route("/switch/:id").put((req, res) => {
   const newData = req.body;
