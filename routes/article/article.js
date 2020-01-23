fa715dae19c3ff04d4dd2e47891f231e63496447
Morgan-Leblanc <morgan.leblanc91@gmail.com>
2020-01-23 15:42:31 +0100

    modif add article

--- routes/article/article.js
+++ routes/article/article.js
@@ -17,14 +17,14 @@ router.get("/", (req, res) => {
   );
 });
 
-// 
+//
 // router.get("/subcat/:id", (req, res) => {
 //   const idSubCat = req.params.id;
 //   connection.query(
 //     "SELECT * from Article  left join Users on Article.id_user_vendeur=Users.id left join Users as U on Article.id_user_acheteur=U.id left join  Photos on Article.id= Photos.id left join Categorie on Article.id = Categorie.id left join Sub_categorie on Article.id =Sub_categorie.id left join Article_gender on Article.id = Article_gender.id_article left join Article_age on Article.id = Article_age.id_article WHERE id_subcategorie = ?",
 // get all article from subCat
 router.get("/subcat/:id", (req, res) => {
-  const idSubCat = req.params.id
+  const idSubCat = req.params.id;
   connection.query(
     "SELECT * FROM Article_subcategorie JOIN Article ON Article_subcategorie.id_article = Article.id WHERE Article_subcategorie.id_subcategorie = ?",
     idSubCat,
@@ -60,14 +60,16 @@ router.get("/:id", (req, res) => {
 
 router.post("/search", (req, res) => {
   const name = req.body.name;
-  console.log(req.body)
+  console.log(req.body);
 
   connection.query("SELECT * FROM Article", (err, results) => {
     if (err) {
       res.status(500).send(`Erreur lors de la récupération de l'article!`);
     } else {
       // console.log(results)
-      const resFilter = results.filter(elem => elem.name.toLowerCase().indexOf(name.toLowerCase()) > -1   )
+      const resFilter = results.filter(
+        elem => elem.name.toLowerCase().indexOf(name.toLowerCase()) > -1
+      );
       res.status(200).json(resFilter);
     }
   });
@@ -75,125 +77,155 @@ router.post("/search", (req, res) => {
 
 //////////////////////Gérer un article/////////////////////
 router.post("/addarticle", (req, res) => {
-  console.log(
-    "je suis la",
-    req.body
-  );
+  //       const photoInfo = req.body.photos
+  //       connection.query("INSERT INTO Photos SET ?", photoInfo, (err, results) => {
+  //         if(err) {
+  //           res.send("Erreur")
+  //         }
+  //       })
   const body = req.body.article;
+  // console.log("=============================" , { ...body, id_photoart: "niqsdkqksd" })
   connection.beginTransaction(err => {
     connection.query("SET FOREIGN_KEY_CHECKS = 0", body, (err, results) => {
       if (err) {
-        console.log("err 00 -- ", err);        
+        console.log("err 00 -- ", err);
         res.send("Erreur.").status(500);
       } else {
-        connection.query(`INSERT INTO Article SET ?`, body, (err, results) => {
-          if (err) {
-            console.log("err 01 -- ", err);
-            res.send(err.message);
-          } else {
-            const categoriesId = req.body.categories;
-            const articleId = results.insertId;
-            console.log(articleId);
+        const urlPhotos = req.body.article.id_photoart;
+        connection.query(
+          "INSERT INTO Photos SET ? ",urlPhotos,
+          (err, results) => {
+            if (err) {
+              res.send("Erreur");
+            }
+            else{
+              const idPhotos = results
+              console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",results)
 
-            const articleCategorie = categoriesId.map(id => {
-              const line = [id, articleId];
-              return line;
-            });
-            console.log(articleCategorie);
-
+            }
+            const bodyModif = { ...body, id_photoart: idPhotos }
+            console.log(bodyModif)
             connection.query(
-              "INSERT INTO `Article_categorie`(`id_categorie`, `id_article`) VALUES ?",
-              [articleCategorie],
-              (err, result) => {
-                console.log(result);
-
+              `INSERT INTO Article SET ?`,
+              bodyModif,
+              (err, results) => {
                 if (err) {
-                  console.log("err 02 -- ", err);
-
-                  connection.rollback(() => {
-                    throw err;
-                  });
+                  console.log("err 01 -- ", err);
                   res.send(err.message);
                 } else {
-                  const subcategoriesId = req.body.subcategories;
-                  const articleSubcategorie = subcategoriesId.map(id => {
+                  const categoriesId = req.body.categories;
+                  const articleId = results.insertId;
+                  console.log(articleId);
+
+                  const articleCategorie = categoriesId.map(id => {
                     const line = [id, articleId];
                     return line;
                   });
+                  console.log(articleCategorie);
 
                   connection.query(
-                    "INSERT INTO `Article_subcategorie`(`id_subcategorie`, `id_article`) VALUES ? ",
-                    [articleSubcategorie],
-                    err => {
+                    "INSERT INTO `Article_categorie`(`id_categorie`, `id_article`) VALUES ?",
+                    [articleCategorie],
+                    (err, result) => {
+                      console.log(result);
+
                       if (err) {
-                        console.log("sub cat");
-                        console.log("err 03 -- ", err);
+                        console.log("err 02 -- ", err);
+
                         connection.rollback(() => {
                           throw err;
                         });
                         res.send(err.message);
                       } else {
-                        const gendersId = req.body.genders;
-                        const articleGenders = gendersId.map(id => {
+                        const subcategoriesId = req.body.subcategories;
+                        const articleSubcategorie = subcategoriesId.map(id => {
                           const line = [id, articleId];
                           return line;
                         });
 
                         connection.query(
-                          "INSERT INTO `Article_gender`(`id_gender`, `id_article`) VALUES ? ",
-                          [articleGenders],
+                          "INSERT INTO `Article_subcategorie`(`id_subcategorie`, `id_article`) VALUES ? ",
+                          [articleSubcategorie],
                           err => {
                             if (err) {
-                              console.log("gender err");
-                              console.log("err 04 -- ", err);
+                              console.log("sub cat");
+                              console.log("err 03 -- ", err);
                               connection.rollback(() => {
                                 throw err;
                               });
                               res.send(err.message);
                             } else {
-                              const agesId = req.body.ages;
-                              const articleAges = agesId.map(id => {
+                              const gendersId = req.body.genders;
+                              const articleGenders = gendersId.map(id => {
                                 const line = [id, articleId];
                                 return line;
                               });
+
                               connection.query(
-                                "INSERT INTO `Article_age`(`id_age`, `id_article`) VALUES ?",
-                                [articleAges],
+                                "INSERT INTO `Article_gender`(`id_gender`, `id_article`) VALUES ? ",
+                                [articleGenders],
                                 err => {
                                   if (err) {
-                                    console.log("age err");
-                                    console.log("err 05 -- ", err);
+                                    console.log("gender err");
+                                    console.log("err 04 -- ", err);
                                     connection.rollback(() => {
                                       throw err;
                                     });
                                     res.send(err.message);
                                   } else {
-                                    connection.commit(err => {
-                                      if (err) {
-                                        console.log("poil");
-                                        console.log("err 06 -- ", err);
-                                        connection.rollback(() => {
-                                          throw err;
-                                        });
-                                      } else {
-                                        connection.query(
-                                          "SET FOREIGN_KEY_CHECKS=1",
-                                          (err, results) => {
-                                            if (err) {
-                                              console.log("err 07 -- ", err);
-                                              res
-                                                .send(
-                                                  "Erreur lors de l'ajout du produit."
-                                                )
-                                                .status(500);
-                                            }
-                                            res.status(200).send("gooood");
-                                            console.log("salut les zouzous");
-                                            // connection.end();
-                                          }
-                                        );
-                                      }
+                                    const agesId = req.body.ages;
+                                    const articleAges = agesId.map(id => {
+                                      const line = [id, articleId];
+                                      return line;
                                     });
+                                    connection.query(
+                                      "INSERT INTO `Article_age`(`id_age`, `id_article`) VALUES ?",
+                                      [articleAges],
+                                      err => {
+                                        if (err) {
+                                          console.log("age err");
+                                          console.log("err 05 -- ", err);
+                                          connection.rollback(() => {
+                                            throw err;
+                                          });
+                                          res.send(err.message);
+                                        } else {
+                                          connection.commit(err => {
+                                            if (err) {
+                                              console.log("poil");
+                                              console.log("err 06 -- ", err);
+                                              connection.rollback(() => {
+                                                throw err;
+                                              });
+                                            } else {
+                                              connection.query(
+                                                "SET FOREIGN_KEY_CHECKS=1",
+                                                (err, results) => {
+                                                  if (err) {
+                                                    console.log(
+                                                      "err 07 -- ",
+                                                      err
+                                                    );
+                                                    res
+                                                      .send(
+                                                        "Erreur lors de l'ajout du produit."
+                                                      )
+                                                      .status(500);
+                                                  }
+                                                  res
+                                                    .status(200)
+                                                    .send("gooood");
+                                                  console.log(
+                                                    "salut les zouzous"
+                                                  );
+                                                  // connection.end();
+                                                }
+                                              );
+                                            }
+                                          });
+                                        }
+                                      }
+                                    );
                                   }
                                 }
                               );
@@ -207,28 +239,28 @@ router.post("/addarticle", (req, res) => {
               }
             );
           }
-        });
+        );
       }
     });
   });
 });
 
-router.route("/switch/:id").put((req, res) => {
-  const newData = req.body;
-  const id = req.params.id;
-  connection.query(
-    "UPDATE  Article  SET ? WHERE id = ? ",
-    [newData, id],
-    (err, results) => {
-      console.log(err);
-      if (err) {
-        res.status(500).send(`Erreur lors de la modification de l'article !`);
-      } else {
-        res.status(200).json(results);
-      }
-    }
-  );
-});
+// router.route("/switch/:id").put((req, res) => {
+//   const newData = req.body;
+//   const id = req.params.id;
+//   connection.query(
+//     "UPDATE  Article  SET ? WHERE id = ? ",
+//     [newData, id],
+//     (err, results) => {
+//       console.log(err);
+//       if (err) {
+//         res.status(500).send(`Erreur lors de la modification de l'article !`);
+//       } else {
+//         res.status(200).json(results);
+//       }
+//     }
+//   );
+// });
 
 router.route("/delete/:id").delete((req, res) => {
   const id = req.params.id;
