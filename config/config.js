2a9809ba2bc88e1f3b36d0833a059d47fe6fa7a2
Morgan-Leblanc <morgan.leblanc91@gmail.com>
2020-02-06 15:12:41 +0100

    end of modif for WCS students see you soon

--- config/config.js
+++ config/config.js
@@ -1,20 +1,18 @@
-const mysql = require('mysql');
-
+const mysql = require('mysql')
 require('dotenv').config(process.cwd(), '.env')
-
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PWD,
     database: process.env.DB_DB,
-});
-
-connection.connect(err => {
-  if (!err) {
-    console.log("Database is connected")
-  } else {
-    console.log("Error connecting database", err)
-  }
 })
+
+connection.connect((err) => {
+    if (!err) {
+        console.log(`😎😎 -- MySql is connected -- 😎😎`);
+    } else {
+        console.log("-- 👎 👎  -- Error connecting MySql : -- 👎 👎 -- ", err);
+    }
+});
 
 module.exports = connection
