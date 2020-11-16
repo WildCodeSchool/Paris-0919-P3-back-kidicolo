a9d68647c9027902e7790ff3e55a632e011f47fb
Timotheediot <timotheediot@gmail.com>
2020-11-16 17:02:59 +0100

    add config

--- config/config.js
+++ config/config.js
@@ -1,18 +1,19 @@
-const mysql = require('mysql')
-require('dotenv').config(process.cwd(), '.env')
+const mysql = require("mysql");
+require("dotenv").config(process.cwd(), ".env");
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    database: process.env.DB_DB,
-})
-
-connection.connect((err) => {
-    if (!err) {
-        console.log(`😎😎 -- MySql is connected -- 😎😎`);
-    } else {
-        console.log("-- 👎 👎  -- Error connecting MySql : -- 👎 👎 -- ", err);
-    }
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  database: process.env.DB_DB,
+  port: process.env.DB_PORT,
 });
 
-module.exports = connection
+connection.connect((err) => {
+  if (!err) {
+    console.log(`😎😎 -- MySql is connected -- 😎😎`);
+  } else {
+    console.log("-- 👎 👎  -- Error connecting MySql : -- 👎 👎 -- ", err);
+  }
+});
+
+module.exports = connection;
